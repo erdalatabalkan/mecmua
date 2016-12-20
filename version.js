7269e65e252f120b5dfd93422431fc3f2ee7aa1d
erdalatabalkan <erdalatabalkan@live.com>
2016-12-21 00:57:17 +0200

    Update version.js

--- version.js
+++ version.js
@@ -1,36 +1,39 @@
-function getUrlVars(){
-	var vars = [], hash;
-	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-	for(var i = 0; i < hashes.length; i++){
-		hash = hashes[i].split('=');
-		if($.inArray(hash[0], vars)>-1){
-			vars[hash[0]]+=","+hash[1];
+$(document).ready(function() {
+
+	function getUrlVars(){
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++){
+			hash = hashes[i].split('=');
+			if($.inArray(hash[0], vars)>-1){
+				vars[hash[0]]+=","+hash[1];
+			}
+			else{
+				vars.push(hash[0]);
+				vars[hash[0]] = hash[1];
+			}
 		}
-		else{
-			vars.push(hash[0]);
-			vars[hash[0]] = hash[1];
+		return vars;
+	}
+	
+	function Bground(bglink) {
+		document.body.style.background = "#f3f3f3 url('"+bglink+"apk/ic_splash.jpg')";
+	}
+
+	function Toast(t1,t2) {
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			Android.verToast(t1);
+			Android.linkToast(t2);
 		}
 	}
-	return vars;
-}
 
-function Bground(bglink) {
-	document.body.style.background = "#f3f3f3 url('"+bglink+"apk/ic_splash.jpg')";
-}
+	var paket 	= getUrlVars()["pak"];
+	//document.write(paket);
 
-function Toast(t1,t2) {
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		Android.verToast(t1);
-		Android.linkToast(t2);
+	if(paket == "erd.mecmua") {
+		var ver		= "1.01.0";
+		var link	= "http://mecmua.ga/";
+		Bground(link);
+		Toast(ver,link);
 	}
-}
-
-var paket 	= getUrlVars()["pak"];
-//document.write(paket);
-
-if(paket == "erd.mecmua") {
-	var ver		= "1.01.0";
-	var link	= "http://mecmua.ga/";
-	Bground(link);
-	Toast(ver,link);
-}
+});
