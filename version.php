d26f5eca5d55aade7ab147775c82c0420b0a4460
erdalatabalkan <erdalatabalkan@live.com>
2016-12-20 16:51:51 +0200

    Update version.php

--- version.php
+++ version.php
@@ -2,3 +2,24 @@
 <isim></isim>
 <ver></ver>
 </app>
+
+<?php $ver	= "1043"; ?>
+
+<style>
+body, html, * {
+	margin: 0px;
+	padding: 0px;
+	background-image: url(gorsel/ic_splash.jpg);
+	background-position: center center;
+	background-repeat: no-repeat;
+	background-size: 100% 100%;
+	width: 100%;
+	height: 100%;
+}
+</style>
+
+<script>
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  Android.showToast("<?php echo $ver; ?>");
+}
+</script>
