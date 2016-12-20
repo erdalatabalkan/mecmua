792fada110579442d8bbb4b74727eb86dace510d
erdalatabalkan <erdalatabalkan@live.com>
2016-12-21 01:01:10 +0200

    Update version.js

--- version.js
+++ version.js
@@ -17,7 +17,8 @@ $(document).ready(function() {
 	}
 	
 	function Bground(bglink) {
-		document.body.style.background = "#f3f3f3 url('"+bglink+"apk/ic_splash.jpg')";
+		document.body.style.backgroundColor = "#f3f3f3";
+		document.body.style.backgroundImage = "url('"+bglink+"apk/ic_splash.jpg')";
 	}
 
 	function Toast(t1,t2) {
