aff79e0ea7fbe9336fcca452681345b10227dae6
erdalatabalkan <erdalatabalkan@live.com>
2016-12-21 19:55:02 +0200

    Update splash.js

--- splash.js
+++ splash.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 	}
 
 	if( splashpacket == "erd.mecmua" ) {
-		var splashversion = "1.01.1";
+		var splashversion = "1.01.5";
 		var splashlink	  = "mecmua.ga";
 		var splashcolor   = "#f3f3f3";
 		splashbackground(splashcolor);
